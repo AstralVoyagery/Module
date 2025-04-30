290781c975611d63369a2103fdc51d86f79fde29
Marvin <Pioneers.Giant@gmail.com>
2025-04-30 15:46:40 +0800

    automatically obtain cookie

--- script/nodeseek.js
+++ script/nodeseek.js
@@ -1,11 +1,11 @@
-let args = {};
-
-if (!$argument.includes("填入")) {
-  args = Object.fromEntries($argument.split(',').map(item => item.split('&')));
-} else {
-  $notification.post("获取cookie失败 ❌", "", "请在模块中配置cookie");
+if (typeof $request !== 'undefined') {
+  $persistentStore.write($request.headers.cookie, "nodeseek_cookie");
+  $notification.post("Cookie获取/更新成功✅", "", "");
   $done();
 }
+
+let args = {};
+if (typeof $argument !== 'undefined') args = Object.fromEntries($argument.split(',').map(item => item.split('&')));
 
 const request = {
   url: `https://www.nodeseek.com/api/attendance?random=${args.NS_RANDOM}`,
@@ -13,7 +13,7 @@ const request = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
     'Origin': 'https://www.nodeseek.com',
     'Referer': 'https://www.nodeseek.com/board',
-    'Cookie': args.NS_COOKIE
+    'Cookie': $persistentStore.read("nodeseek_cookie")
   }
 }
 
@@ -25,9 +25,10 @@ $httpClient.post(request, (error, response, data) => {
   } else if (result.message.includes("完成")) {
     $notification.post("NodeSeek签到 ⚠️", "", result.message)
   } else if (result.message == "USER NOT FOUND") {
-    $notification.post("NodeSeek签到 ⚠️", "", "Cookie不正确/已失效，请重新获取")
+    $notification.post("NodeSeek签到 ⚠️", "", "Cookie未获取/已失效，请重新获取")
   } else {
     $notification.post("NodeSeek签到 ❌", "", result.message || "未知错误")
   }
+
   $done();
 });
